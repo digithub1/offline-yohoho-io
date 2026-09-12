d3e270d2955971d97c3e5d10578a8c28cc122172
digithub1 <digithub1@users.noreply.github.com>
2026-09-12 21:39:44 +0530

    Initial clean offline commit

---
+++ adSense.js
@@ -0,0 +1,11 @@
+// Google AdSense SDK Placeholder
+// Uncomment and update with your publisher ID when ready to enable ads:
+/*
+(function() {
+    let script = document.createElement("script");
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=YOUR_PUBLISHER_ID";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+})();
+*/
